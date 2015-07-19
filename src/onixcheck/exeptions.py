5a36bfb8bb8eceab57203387072f3bf492b2a418
titusz <tp@py7.de>
2015-07-20 00:59:55 +0200

    Add NullHandler to silence errors when logging without configuration

--- src/onixcheck/exeptions.py
+++ src/onixcheck/exeptions.py
@@ -1,5 +1,20 @@
 # -*- coding: utf-8 -*-
+import logging
 
 
 class OnixError(Exception):
     pass
+
+
+class NullHandler(logging.Handler):
+    """Not in python 2.6 so we use our own"""
+
+    def emit(self, record):
+        pass
+
+
+def get_logger(logger_name='onixcheck', add_null_handler=True):
+    logger = logging.getLogger(logger_name)
+    if add_null_handler:
+        logger.addHandler(NullHandler())
+    return logger
