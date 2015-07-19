43df0ee05a5f0b2540a838750760333bae2257f5
titusz <tp@py7.de>
2015-07-19 15:16:48 +0200

    onixcheck can take file object or file path

--- src/onixcheck/validation.py
+++ src/onixcheck/validation.py
@@ -7,6 +7,7 @@ from onixcheck.models import OnixFile, Message
 def validate(infile):
     """Validatate python file
 
+    :param file or path infile: File Obj or path to file
     :return list: List Message objs (invalid onix) or empty list (valid onix)
     """
     onix_file = OnixFile(infile)
@@ -14,4 +15,10 @@ def validate(infile):
     validator(onix_file.xml_tree())
     errors = validator.error_log
     msg = Message.from_logentry
-    return [msg(e, basename(infile)) for e in errors]
+
+    if hasattr(infile, 'name'):
+        filename = infile.name
+    else:
+        filename = basename(infile)
+
+    return [msg(e, filename)for e in errors]
--- tests/test_validation.py
+++ tests/test_validation.py
@@ -3,6 +3,13 @@ import onixcheck
 from onixcheck import data
 
 
-def test_validation_02_ref_valid():
+def test_validation_with_file_path():
     errors = onixcheck.validate(data.VALID_ONIX2_REF)
     assert errors == []
+
+
+def test_validation_with_file_obj():
+    with open(data.VALID_ONIX3_REF, 'rb') as onix_file:
+        errors = onixcheck.validate(onix_file)
+    assert not errors
+
