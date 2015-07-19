a8eb1aa99cafdc42797e5cd5b960920b2cd9cb91
titusz <tp@py7.de>
2015-07-19 15:34:00 +0200

    Don´t mess with test output

--- tests/test_onixcheck.py
+++ tests/test_onixcheck.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from __future__ import unicode_literals
 from click.testing import CliRunner
 from onixcheck.__main__ import main
 from onixcheck import data
@@ -21,19 +22,19 @@ def test_main_o2_ref_valid():
 def test_main_has_ns_valid():
     runner = CliRunner()
     result = runner.invoke(main, [data.VALID_ONIX3_REF_NS])
-    assert 'validating' in result.output.lower()
+    assert 'Validating' in result.output
     assert result.exit_code == 0
 
 
 def test_main_plain_invalid():
     runner = CliRunner()
     result = runner.invoke(main, [data.INVALID_ONIX3_REF])
-    assert 'error' in result.output.lower()
+    assert 'ERROR' in result.output
     assert result.exit_code == 1
 
 
 def test_main_debug():
     runner = CliRunner()
     result = runner.invoke(main, [data.VALID_ONIX3_REF, '-d'])
-    assert 'debug' in result.output.lower()
+    assert 'DEBUG' in result.output
     assert result.exit_code == 0
